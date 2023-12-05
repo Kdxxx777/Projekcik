fad54eade70dce034d6769296f8d53020150db45
KDX <hubert.galicki.ext@camline.com>
2023-12-05 19:47:00 +0100

    Stworzenie metody placeOrderOnList, Test w klasie Main i dodanie Nowej klasy Test

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,7 +1,17 @@
 package test;
 
+import entity.Order;
+import service.OrderService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Order order = new Order(1, 3, Map.of(1,1), 10d);
+        OrderService orderService = new OrderService();
+        orderService.placeOrderOnList(order);
     }
 }
