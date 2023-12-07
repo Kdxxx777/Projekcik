5d2a7f2d3a7fce3e4c7928681765a02e530029c1
Wikanter <kacper.kowalewsky@gmail.com>
2023-12-07 20:19:11 +0100

    Przetestowano dodatkowe gettery z klasy Fruits w klasie Main po pierwszym review

--- src/test/Main.java
+++ src/test/Main.java
@@ -11,11 +11,15 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         System.out.println("To ja Fabian.");
-        Order order = new Order(1, 3, Map.of(1,1), 10d);
+        Order order = new Order(1, 3, Map.of(1, 1), 10d);
         OrderService orderService = new OrderService();
         orderService.placeOrderOnList(order);
 
-        Fruits fruits = new Fruits(1,"Jablko",15.99,1,"Afryka");
+        Fruits fruits = new Fruits(1, "Jablko", 15.99, 1, "Afryka");
         System.out.println(fruits.getCountryOfOrigin());
+        String nameOfFruit = fruits.getName();
+        System.out.println(nameOfFruit);
+        System.out.println("cena " + nameOfFruit + " wynosi: " + fruits.getPrice());
+
     }
 }
