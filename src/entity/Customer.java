7bd3d34717310a651ec3fc2b37de19ccf9b16d66
FKFabian <fabian.klocek@gmail.com>
2023-12-07 18:44:32 +0100

    Dodanie zmiennej discount do klasy customer.

--- src/entity/Customer.java
+++ src/entity/Customer.java
@@ -5,20 +5,14 @@ public class Customer {
     private String nickName;
     private String address;
     private int age;
+    private double discount;
 
-    public Customer(int customerId, String nickName, String address, int age) {
+    public Customer(int customerId, String nickName, String address, int age, double discount) {
         this.customerId = customerId;
         this.nickName = nickName;
         this.address = address;
         this.age = age;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+        this.discount = discount;
     }
 
     public int getCustomerId() {
@@ -43,5 +37,32 @@ public class Customer {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", nickName='" + nickName + '\'' +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                ", discount=" + discount +
+                '}';
     }
 }
