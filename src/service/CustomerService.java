4184ced950d736188f1de1d7ddb55d5973b9f6ad
Robert Pietraszewski <80597@student.pb.edu.pl>
2024-01-12 14:28:39 +0100

    Dodanie klasy serwisowej CustomerService z metodami dodawania i wybierania pojedyńczego Customera

---
+++ src/service/CustomerService.java
@@ -0,0 +1,23 @@
+package service;
+
+import entity.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerService {
+    private List<Customer> customers = new ArrayList<>();
+
+    public void addCustomer(Customer customer){
+        customers.add(customer);
+    }
+
+    public Customer getCustomer(int customerId){
+        for (Customer customer : customers) {
+            if (customer.getCustomerId() == customerId){
+                return customer;
+            }
+        }
+        return null;
+    }
+}
