9165b6527e4c2c42a8b34a3d2bc98f9f340a3c76
Kdxxx777 <152989593+Kdxxx777@users.noreply.github.com>
2024-03-14 20:33:40 +0100

    Revert "Souts"

--- src/test/Main.java
+++ src/test/Main.java
@@ -12,10 +12,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("To ja Fabian.");
         System.out.println("To ja Kamil");
-        System.out.print("test2");
-        System.out.println("abc");
-        System.out.println("To ja Alex");
-        Test.hiAlex();
+        System.out.printf("test2");
         Order order = new Order(1, 3, Map.of(1,1), 10d);
         Order order1 = new Order(3, 2, Map.of(1,2), 200);
         OrderService orderService = new OrderService();
