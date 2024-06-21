45ddcba50fff410a5ced7539a04ca358b6abc7ca
pmarko <p.marko09@yahoo.com>
2024-06-21 08:55:00 +0200

    field timestamp added and set in constructor VegetableService

--- src/service/VegetableService.java
+++ src/service/VegetableService.java
@@ -2,15 +2,19 @@ package service;
 
 import entity.Vegetable;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-public class  VegetableService {
+public class VegetableService {
 
     private Map<Integer, Vegetable> vegetables;
+    private LocalDateTime timestamp;
 
     public VegetableService() {
         this.vegetables = new HashMap<>();
+        this.timestamp = LocalDateTime.now();
     }
 
     public void addVegetable(Vegetable vegetable) {
@@ -25,9 +29,10 @@ public class  VegetableService {
         return vegetables;
     }
 
-    public void showVegetables (Map<Integer, Vegetable> vegetableMap) {
+    public void showVegetables(Map<Integer, Vegetable> vegetableMap) {
         for (Map.Entry<Integer, Vegetable> entry : vegetableMap.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 }
+
