9b824223f21eff9af7d3d302671aacc96e88b1d0
budzikovy <budzikovy@gmail.com>
2024-06-21 08:57:16 +0200

    add TimeStamp field and change constructor

--- src/service/FruitsService.java
+++ src/service/FruitsService.java
@@ -4,14 +4,18 @@ import entity.Fruit;
 import entity.Order;
 import entity.Vegetable;
 
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
 
 public class FruitsService {
     private Map<Integer, Fruit> fruits;
+    private Timestamp timestamp;
+
 
     public FruitsService() {
         this.fruits = new HashMap<>();
+        this.timestamp = new Timestamp(System.currentTimeMillis());
     }
 
     public void addFruits(Fruit fruit) {
@@ -25,5 +29,9 @@ public class FruitsService {
         for (Map.Entry<Integer, Fruit> entry : fruits.entrySet()){
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
     }
 }
