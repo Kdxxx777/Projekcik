93ee78a86e0a5b105cf327ad5c0def86a134492a
budzikovy <budzikovy@gmail.com>
2024-06-21 08:47:55 +0200

    add order2 object and call placeOrderOnList on it

--- src/test/Main.java
+++ src/test/Main.java
@@ -15,9 +15,11 @@ public class Main {
         System.out.printf("test2");
         Order order = new Order(1, 3, Map.of(1,1), 10d);
         Order order1 = new Order(3, 2, Map.of(1,2), 200);
+        Order order2 = new Order(2, 4, Map.of(1,3), 20d);
         OrderService orderService = new OrderService();
         orderService.placeOrderOnList(order);
         orderService.placeOrderOnList(order1);
+        orderService.placeOrderOnList(order2);
         System.out.println("Hello, user");
         System.out.println("Good Morning");
         System.out.println("Have a good day!");
